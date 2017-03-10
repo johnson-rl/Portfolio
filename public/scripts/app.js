f7ef0429eeb6d44e142fd54a325555ffa4a3f8c1
johnson-rl <johnson.rl@gmail.com>
2017-03-10 15:26:55 -0800

    added working buttons to bottom section

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,4 +14,28 @@ $(document).ready(function(){
      $('.slider').slider('prev');
    })
 
+   $('.about-button').click(function (event){
+     console.log('clicked')
+     event.preventDefault()
+     $('.contact').toggleClass('hide', true)
+     $('.adventure').toggleClass('hide', true)
+     $('.about').toggleClass('hide', false)
+   })
+
+   $('.contact-button').click(function (event){
+     console.log('clicked')
+     event.preventDefault()
+     $('.about').toggleClass('hide', true)
+     $('.adventure').toggleClass('hide', true)
+     $('.contact').toggleClass('hide', false)
+   })
+
+   $('.adventure-button').click(function (event){
+     console.log('clicked')
+     event.preventDefault()
+     $('.contact').toggleClass('hide', true)
+     $('.about').toggleClass('hide', true)
+     $('.adventure').toggleClass('hide', false)
+   })
+
 });
